a561fe0f5fafb561c6d5ee620fd21244a7388d2e
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2026-05-02 06:39:40 +0300

    Added auto-superuser creation to build script

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# NEW: Automatically create superuser if it doesn't exist
+python manage.py createsuperuser --noinput || true
